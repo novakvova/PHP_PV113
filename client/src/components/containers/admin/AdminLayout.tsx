b0d1495b84af249d4caf32707231f9ed578fb97d
novakvova <novakvova@gmail.com>
2024-04-09 21:00:36 +0300

    Show footer in bottom

--- client/src/components/containers/admin/AdminLayout.tsx
+++ client/src/components/containers/admin/AdminLayout.tsx
@@ -5,9 +5,9 @@ import {Outlet} from 'react-router-dom';
 const AdminLayout = () => {
     return (
       <>
-          <div className="font-body">
+          <div className="flex min-h-screen flex-col font-body">
               <Sidebar />
-              <div className="md:ml-64">
+              <div className="flex flex-grow flex-col justify-between px-6 md:ml-64">
                   <div className={"px-3 py-6"}>
                       <div className={"container max-w-full items-center md:pl-10 md:pr-8"}>
                         <Outlet/>
