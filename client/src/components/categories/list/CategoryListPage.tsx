79f855a33cb18042937e27889dcb3c8c8b4d064d
novakvova <novakvova@gmail.com>
2024-04-09 20:46:53 +0300

    Show list categories

--- client/src/components/categories/list/CategoryListPage.tsx
+++ client/src/components/categories/list/CategoryListPage.tsx
@@ -1,30 +1,27 @@
 import CategoryItem from "./CategoryItem.tsx";
+import {useEffect, useState} from "react";
+import {ICategory} from "../../../interfaces/category";
+import axios from "axios";
 
 const CategoryListPage = () => {
+    const [categories, setCategories] = useState<ICategory[]>([]);
+
+    useEffect(() => {
+        axios.get<ICategory[]>('http://laravel.pv113.com/api/categories')
+            .then(response => {
+                setCategories(response.data);
+            })
+            .catch(error => {
+                console.error('Error fetching categories:', error);
+            });
+    }, []);
+
     return (
         <>
             <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 md:grid-cols-2 xl:grid-cols-3 2xl:grid-cols-4">
-                <div>
-                    <CategoryItem/>
-                </div>
-                <div>
-                    <CategoryItem/>
-                </div>
-                <div>
-                    <CategoryItem/>
-                </div>
-                <div>
-                    <CategoryItem/>
-                </div>
-                <div>
-                    <CategoryItem/>
-                </div>
-                <div>
-                    <CategoryItem/>
-                </div>
-                <div>
-                    <CategoryItem/>
-                </div>
+                {categories.map(category => (
+                    <CategoryItem key={category.id} {...category} />
+                ))}
             </div>
         </>
     );
