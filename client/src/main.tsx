b8231b2c54d3e7546b5afe1fe3df00a7b4012ec4
novakvova <novakvova@gmail.com>
2024-04-11 20:47:26 +0300

    Begin working create new

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -3,12 +3,15 @@ import App from './App.tsx'
 import './index.css'
 import {BrowserRouter} from "react-router-dom";
 import {Provider} from "react-redux";
+import "react-toastify/dist/ReactToastify.css";
 import {store} from "./store";
+import {ToastContainer} from "react-toastify";
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
     <Provider store={store}>
         <BrowserRouter>
             <App/>
+            <ToastContainer />
         </BrowserRouter>
     </Provider>,
 )
