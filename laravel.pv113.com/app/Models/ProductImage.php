cbc6616e2c71c6c994a76d27187defed15d9767a
novakvova <novakvova@gmail.com>
2024-04-18 19:02:27 +0300

    Add model products

--- laravel.pv113.com/app/Models/ProductImage.php
+++ laravel.pv113.com/app/Models/ProductImage.php
@@ -5,21 +5,19 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-/**
- * @method static create(array $inputs)
- * @method static findOrFail($id)
- */
-class Categories extends Model
+class ProductImage extends Model
 {
+    protected $table = 'product_images';
     use HasFactory;
-
     protected $fillable = [
         'name',
-        'image'
+        'priority',
+        'product_id',
     ];
 
-    public function products()
+    public function product()
     {
-        return $this->hasMany(Product::class);
+        return $this->belongsTo(Product::class);
     }
+
 }
